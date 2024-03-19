fe0a144141a3213d2c9f512bc0259c1cc189400f
obynonwane <biostechnologyng@gmail.com>
2024-03-19 18:20:19 +0100

    update

--- crypto/keys.go
+++ crypto/keys.go
@@ -14,15 +14,16 @@ const (
 )
 
 type PrivateKey struct {
-	Key ed25519.PrivateKey
+	key ed25519.PrivateKey
 }
 
-func (p *PrivateKey) GeneratePrivateKey() *PrivateKey {
+// generate private key
+func GeneratePrivateKey() *PrivateKey {
 
 	//make me a empty seed of byte of length of seedLen
 	seed := make([]byte, seedLen)
 
-	//populate the empty byte of seed above while panic if error exist
+	//read from io reader untill buffer is full i.e populates the empty byte of seed above while panic if error exist
 	_, err := io.ReadFull(rand.Reader, seed)
 	if err != nil {
 		panic(err)
@@ -30,31 +31,48 @@ func (p *PrivateKey) GeneratePrivateKey() *PrivateKey {
 
 	//return the generated private key
 	return &PrivateKey{
-		Key: ed25519.NewKeyFromSeed(seed),
+		key: ed25519.NewKeyFromSeed(seed),
 	}
 }
 
 func (p *PrivateKey) Bytes() []byte {
-	return p.Key
+	return p.key
 }
 
-func (p *PrivateKey) Sign(msg []byte) []byte {
-	return ed25519.Sign(p.Key, msg)
+func (p *PrivateKey) Sign(msg []byte) *Signature {
+	return &Signature{
+		value: ed25519.Sign(p.key, msg),
+	}
 }
 
-func (p *PublicKey) Public() *PublicKey {
+func (p *PrivateKey) Public() *PublicKey {
 	//make me a byte of length pubKeyLen
 	b := make([]byte, pubKeyLen)
 
 	//copy the bytes in p.key starting from index 32 into byte slice b
-	copy(b, p.Key[32:])
+	copy(b, p.key[32:])
 
 	//return the generated public key
 	return &PublicKey{
-		Key: b,
+		key: b,
 	}
 }
 
 type PublicKey struct {
-	Key ed25519.PublicKey
+	key ed25519.PublicKey
+}
+
+// returns public key byte
+func (p *PublicKey) Bytes() []byte {
+	return p.key
+}
+
+// signature struct
+type Signature struct {
+	value []byte
+}
+
+// verify signature
+func (s *Signature) Verify(pubKey *PublicKey, msg []byte) bool {
+	return ed25519.Verify(pubKey.key, msg, s.value)
 }
--- crypto/keys_test.go
+++ crypto/keys_test.go
@@ -1,7 +1,22 @@
 package crypto
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestGeneratePrivateKey(t *testing.T) {
-	privKey := 
+	privKey := GeneratePrivateKey()
+	assert.Equal(t, len(privKey.Bytes()), privKeyLen)
+
+	pubKey := privKey.Public()
+	assert.Equal(t, len(pubKey.Bytes()), pubKeyLen)
+}
+
+func TestPrivateKeySign(t *testing.T) {
+	//generate private key
+	privKey := GeneratePrivateKey()
+	//generate message byte
+	msg := []byte("foo bar baz")
 }
